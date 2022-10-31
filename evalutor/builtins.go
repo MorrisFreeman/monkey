0709e29cddce4a9bb5bdd7fe1ef28865c682e635
MorrisFreeman <morrisfreeman0718@gmail.com>
2022-10-31 11:35:03 +0900

    implement puts

--- evalutor/builtins.go
+++ evalutor/builtins.go
@@ -1,6 +1,10 @@
 package evalutor
 
-import "github.com/MorrisFreeman/monkey/object"
+import (
+	"fmt"
+
+	"github.com/MorrisFreeman/monkey/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": &object.Builtin{
@@ -91,6 +95,15 @@ var builtins = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"puts": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
 }
